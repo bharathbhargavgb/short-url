d5e4274be9246d0c407e5ab3fb3737f496ed3e0b
Bharath Bhargav <baaskab@amazon.com>
2021-04-23 23:04:23 +0530

    Add more UTs to cover failure cases

--- server/db_test.go
+++ server/db_test.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+    "fmt"
     "testing"
+    "errors"
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
     "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
 type fakeDynamoDBClient struct {
     dynamodbiface.DynamoDBAPI
+}
+
+type shortItemKey struct {
+    TinyID string
 }
 
 func createFakeStorage() *DBStore {
@@ -19,25 +26,68 @@ func createFakeStorage() *DBStore {
 }
 
 func (m *fakeDynamoDBClient) GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
-    return &dynamodb.GetItemOutput {
-        Item: map[string]*dynamodb.AttributeValue {
-           "TinyID": {
-                S: aws.String("goog"),
-           },
-           "URI": {
-               S: aws.String("https://www.google.com"),
-           },
-        },
-    }, nil
+    shortItem := new(shortItemKey)
+    dynamodbattribute.UnmarshalMap(input.Key, shortItem)
+
+    if shortItem.TinyID == "goog" {
+        return &dynamodb.GetItemOutput {
+            Item: map[string]*dynamodb.AttributeValue {
+                "TinyID": {
+                    S: aws.String("goog"),
+                },
+                "URI": {
+                    S: aws.String("https://www.google.com"),
+                },
+            },
+        }, nil
+    }
+    return nil, errors.New("Key not found")
+}
+
+func (m *fakeDynamoDBClient) PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+    shortItemInput := new(shortURI)
+    err := dynamodbattribute.UnmarshalMap(input.Item, shortItemInput)
+    if err != nil {
+        return nil, errors.New("Unable to unmarshalMap from struct")
+    }
+    if shortItemInput.TinyID == "goog" && shortItemInput.URI == "https://www.google.com" {
+        return nil, nil
+    }
+    return nil, errors.New("Malformed input")
 }
 
 func TestGetItem(t *testing.T) {
     db := createFakeStorage()
     want := "https://www.google.com"
-    shortLink, _ := db.getItem("dummyString")
+    shortLink, err := db.getItem("goog")
+    if err != nil {
+        t.Errorf("getItem returned error: %q", err)
+    }
     if want != shortLink.URI {
-        t.Errorf("got %q, want %q", shortLink.URI, want)
+        t.Errorf("Got %q, Want %q", shortLink.URI, want)
     }
 }
 
+func TestGetItemMissingKey(t *testing.T) {
+    db := createFakeStorage()
+    shortLink, err := db.getItem("randomString")
+    if err == nil {
+        t.Errorf("Want `Key not found`, Got %q", err)
+    }
+    if shortLink != nil {
+        t.Error(fmt.Sprintf("Want nil, Got %+v", *shortLink))
+    }
+}
+
+func TestPutItem(t *testing.T) {
+    db := createFakeStorage()
+    shortItem := &shortURI {
+        TinyID: "goog",
+        URI: "https://www.google.com",
+    }
+    err := db.putItem(shortItem)
+    if err != nil {
+        t.Error("Error inserting to DB")
+    }
+}
 
