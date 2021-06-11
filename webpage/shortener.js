d148b926ad4f1ef4da628fc631a6b20eb5db95f3
Bharath Bhargav <baaskab@amazon.com>
2021-06-11 17:17:33 +0530

    Show loading animation while POST request completes

--- webpage/shortener.js
+++ webpage/shortener.js
@@ -1,25 +1,36 @@
 var baseURL = "https://base.url/"
 $('#url-shortener-form').submit(function(event) {
   event.preventDefault();
+
+  $("#shortID").empty();
+  toggleProgress();
+
   var reqBody = {
     shortID: "",
     URI: $("#url-input").val()
   };
+
   $.ajax({
-      url: 'https://angh4tqiu8.execute-api.us-east-1.amazonaws.com/staging/shorten',
-      type: 'POST',
-      dataType: 'json',
-      data: JSON.stringify(reqBody),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      success: function (data) {
-        var shortURL = baseURL + data.shortID;
-        $("#shortID").html("<a href=\"" + shortURL + "\">" + shortURL + "</a>");
-      },
-      failure: function(data) {
-        console.log("failure " + data);
-      }
+    url: 'https://angh4tqiu8.execute-api.us-east-1.amazonaws.com/staging/shorten',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(reqBody),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    success: function (data) {
+      var shortURL = baseURL + data.shortID;
+      toggleProgress();
+      $("#shortID").html("<a href=\"" + shortURL + "\">" + shortURL + "</a>");
+    },
+    failure: function(data) {
+      toggleProgress();
+      $("#shortID").html("Unable to shorten URL");
+      console.log("failure " + data);
+    }
   });
 });
 
+function toggleProgress() {
+  $(".loader").toggle();
+}
